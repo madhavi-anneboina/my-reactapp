6ea1fcab98212d94f0793b83ff667dd21df3b3de
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-08-09 22:05:27 -0400

    products added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-// import React, { Component } from 'react'
+import React, { Component } from 'react'
 
 
-// class App extends Component {
-//   state = {
-//     product : "Real me pro",
-//     price : 20000
-//   }
+class App extends Component {
+  state = {
+    product : "Real me pro",
+    price : 20000
+  }
   
-//   render() {
-//     return (
-//       <div>
-//         <h1>product:{this.state.product}</h1>
-//         <p>Price: {this.state.price}</p>
-//         <input id="price" type='number'/>
-//         <button Onclick={updatePrice}>Update</button>
-//       </div>
-//     )
-//   }
-// }
+  render() {
+    return (
+      <div>
+        <h1>product:{this.state.product}</h1>
+        <p>Price: {this.state.price}</p>
+        <input id="price" type='number'/>
+        <button Onclick={updatePrice}>Update</button>
+      </div>
+    )
+  }
+}
 
-// export default App;
+export default App;

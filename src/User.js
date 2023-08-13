47d92f7f39905d5a3cfe1ae8d06f4f03883b9a41
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-08-12 22:15:07 -0400

    react class based component

--- src/User.js
+++ src/User.js
@@ -1,28 +1,28 @@
-// // import React from 'react'
+// import React from 'react'
 
-// // const User = ({name}) => {
-// //   return (
-// //     <div>
-// //       <p>user name is : {name}</p>
-// //     </div>
-// //   )
-// // }
-
-// // export default User
-// import React, { Component } from 'react'
-
-//  class User extends Component {
-//   render() {
-//     return (
-//       <div>
-//            <p>user deatils are</p>
-//            <p>{this.props.id}</p>
-//            <p>{this.props.name}</p>
-//            <p>{this.props.age}</p>
-//            <p>{this.props.place}</p>
-//       </div>
-//     )
-//   }
+// const User = ({name}) => {
+//   return (
+//     <div>
+//       <p>user name is : {name}</p>
+//     </div>
+//   )
 // }
-// export default User;
+
+// export default User
+import React, { Component } from 'react'
+
+ class User extends Component {
+  render() {
+    return (
+      <div>
+           <p>user deatils are</p>
+           <p>{this.props.id}</p>
+           <p>{this.props.name}</p>
+           <p>{this.props.age}</p>
+           <p>{this.props.place}</p>
+      </div>
+    )
+  }
+}
+export default User;
 

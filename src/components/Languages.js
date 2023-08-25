899a915e4bfda01b7803b050243eaf6c672f5986
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-08-25 15:06:04 -0400

    language component implemented

---
+++ src/components/Languages.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Languages = ({language,onLanguageChange}) => {
+  return (
+    <div>
+      <label className='label'>
+        select Language
+      </label>
+      <div className='opts'/>
+    </div>
+  )
+}
+
+export default Languages

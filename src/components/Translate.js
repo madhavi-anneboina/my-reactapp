2978fc1b3716aaba57f058396bcb1f9eeb3e9814
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-08-25 15:09:43 -0400

    Translate component added

---
+++ src/components/Translate.js
@@ -0,0 +1,14 @@
+import React,{useTranslation} from 'react'
+
+const Translate = ({text,language}) => {
+    const Translated = useTranslation(text,language)
+  return (
+    <div className='translate'>
+        <label className='label'>Output</label>
+        <h1 className='title'>{Translated}</h1>
+
+    </div>
+  )
+}
+
+export default Translate

11a543c7fa6cb24055bd9e00ad3a1d5029ae1227
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-08-27 15:46:54 -0400

    added profilecard component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 // import './styles.css'
 import React, { useState } from 'react';
-import Filed from './components/Filed';
-import Languages from './components/Languages';
-import Translate from './components/Translate';
+import ProfileCard from './ProfileCard';
 
 
 const App = () => {
-  const[language,setLanguage] = useState('es')
-  const[text,setText] = useState('')
-  // const date = new Date()
-  // const time = date.toLocaleTimeString()
-  const inputType="number"
-  const minValue = 5;
+
   return (
     <div>
-      <textarea autofocus={true}/>
+      <ProfileCard />
     </div>
   )
 }

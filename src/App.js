021c5420a7126192152b4c56fcac40b7643538f6
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-08-27 20:12:16 -0400

    bulma added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // import './styles.css'
+import 'bulma/css/bulma.css'
 import React, { useState } from 'react';
 import ProfileCard from './ProfileCard';
 import alexa from './images/alexa.png';
@@ -9,12 +10,26 @@ import siri from './images/siri.png'
 const App = () => {
 
   return (
-    <div>
-      <h5>Profile App Assistances</h5>
-      <ProfileCard title="Alexa" handle="@alexa99" image={alexa}/>
-      <ProfileCard title="Cortana" handle="@cortana32" image={cortana}/>
-      <ProfileCard title="Siri" handle="@siri01" image={siri} />
+    // <div>
+    //   <h5>Profile App Assistances</h5>
+    //  <ProfileCard title="Alexa" handle="@alexa99" image={alexa}/>
+    //   <ProfileCard title="Cortana" handle="@cortana32" image={cortana}/>
+    //   <ProfileCard title="Siri" handle="@siri01" image={siri} />
+    // </div>
+
+    <div class="columns">
+      <div class="column">
+        <ProfileCard title="Alexa" handle="@alexa99" image={alexa} />
+      </div>
+      <div class="column">
+        <ProfileCard title="Cortana" handle="@cortana32" image={cortana} />
+      </div>
+      <div class="column">
+        <ProfileCard title="Siri" handle="@siri01" image={siri} />
+      </div>
     </div>
+
+
   )
 }
 
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -3,41 +3,53 @@ import React from 'react';
 
 const ProfileCard = ({ title, handle, image }) => {
     return (
-    //     <div class="row">
-    //     {/* <img src={image} /> */}
-    //     <div class="col-sm-6">
-    //         <div class="card" style={{ width: "100%" }}>
-    //             <div class="row no-gutters">
-    //                 <div class="col-md-4">
-    //                     <img class="card-img" src={image} />
-    //                 </div>
-    //                 <div class="col-md-8">
-    //                     <div class="card-body">
-    //                         <h5 class="card-title">Title Is {title}</h5>
-    //                         <p class="card-text">Handle is {handle}</p>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </div>
-    // </div>
-    
-    <div class="row">
-    <div class="col-sm-6">
-        <div class="card" style={{ width: "18rem" }}>
-            <img class="card-img-top" src={image} />
-            <div class="card-body">
-                <h5 class="card-title">Title Is {title}</h5>
-                <p class="card-text">Handle is {handle}</p>
+        //     <div class="row">
+        //     {/* <img src={image} /> */}
+        //     <div class="col-sm-6">
+        //         <div class="card" style={{ width: "100%" }}>
+        //             <div class="row no-gutters">
+        //                 <div class="col-md-4">
+        //                     <img class="card-img" src={image} />
+        //                 </div>
+        //                 <div class="col-md-8">
+        //                     <div class="card-body">
+        //                         <h5 class="card-title">Title Is {title}</h5>
+        //                         <p class="card-text">Handle is {handle}</p>
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+
+        //     <div class="row">
+        //     <div class="col-sm-6">
+        //         <div class="card" style={{ width: "18rem" }}>
+        //             <img class="card-img-top" src={image} />
+        //             <div class="card-body">
+        //                 <h5 class="card-title">Title Is {title}</h5>
+        //                 <p class="card-text">Handle is {handle}</p>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+        <div className="card" style={{ width: "18rem" }}>
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt="Placeholder image"/>
+                </figure>
+            </div>
+            <div className="card-content">
+                    <div className="media-content">
+                        <p className="title is-4"> Title is : {title}</p>
+                        <p className="subtitle is-6">Handle is : {handle}</p>
+                    </div>
             </div>
         </div>
-    </div>
-</div>
-    
-    
-    
-    
-    
+
+
+
+
     )
 }
 

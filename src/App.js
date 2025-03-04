2acc052530cc48540ae186bdf9b82273bfe1ddd0
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2025-03-03 19:20:17 -0500

    added data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 // import './styles.css'
 import 'bulma/css/bulma.css'
 import React, { useState } from 'react';
-import AnimalShow from './AnimalShow';
 
-let getRandomAnimal =() =>{
-  const animals = ["bird","cat","cow","dog","horse","hary"]
-  return animals[Math.floor(Math.random()*animals.length)]
-}
+
+
 
 
 const App = () => {
-  const [animals,setAnimals] = useState([])
-const handleClick = () =>{
- setAnimals([...animals,getRandomAnimal()])
-}
+
   return (
      <div>
-         <button onClick={handleClick}>Add Element</button>
+        
 
-         <div>{animals}</div>
+         <h1>Hello world</h1>
+
       
      </div>
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('should render Hello World text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement  = screen.getByText(/Hello World/i);
+  expect(headingElement ).toBeInTheDocument();
 });
+
+

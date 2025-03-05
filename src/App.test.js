009b378e945dcab726864a773f85c5fab4a762c6
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2025-03-04 22:39:44 -0500

    functional components

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Greet from './components/Greet';
 
 test('should render Hello World text', () => {
   render(<App />);
   const headingElement  = screen.getByText(/Hello World/i);
   expect(headingElement ).toBeInTheDocument();
 });
+
+test('should render Hello how are you text', () => {
+  render(<Greet />);
+  const headingElement  = screen.getByText(/Hello how are you/i);
+  expect(headingElement ).toBeInTheDocument();
+});
+
+
 
 
---
+++ src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Greet = ({name}) =>{
+    return (
+        <div>
+            <h1>Hello {name} how are you</h1>
+        </div>
+    )
+}
+
+export default Greet

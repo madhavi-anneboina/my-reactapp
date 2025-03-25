376df3ac4916908d58bfe25ab08fb42313e86ec7
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2025-03-25 18:47:27 -0400

    added life cycle methods

---
+++ src/components/FragementDemo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FragementDemo = () =>{
+    return(
+        <>
+         <h1>Hello how are you </h1>
+         <p> Iam learning about FragementDemo</p>
+        </>
+    )
+}
+
+export default FragementDemo

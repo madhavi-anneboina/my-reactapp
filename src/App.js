8fb449f67be26b0da9d4dc4583206efb082a5015
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2025-10-13 21:06:41 -0400

    css display properties

--- src/App.js
+++ src/App.js
@@ -106,6 +106,32 @@ const App = () => {
           <HoverCounterTwo count={count} incrementCount={incrementCount} />
         )}
       /> */}
+
+      <table>
+        <tr>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Age</th>
+        </tr>
+        <tr>
+          <td>Jill</td>
+          <td>Smith</td>
+          <td>50</td>
+        </tr>
+        <tr>
+          <td>Eve</td>
+          <td>Jackson</td>
+          <td>94</td>
+        </tr>
+        <tr>
+          <td>John</td>
+          <td>Doe</td>
+          <td>80</td>
+        </tr>
+      </table>
+
+      <div class="content1">Sample content - 1</div>
+      <span class="content2">Sample content - 2</span>
     </div>
   );
 };
